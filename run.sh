be795b0248b59c5d3ca4e341478e0712346801d7
6ministers <manyatovskiy@gmail.com>
2023-12-18 21:20:39 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 DESTINATION=$1
 PORT=$2
 CHAT=$3
-# clone Odoo directory
-git clone --depth=1 https://github.com/minhng92/odoo-17-docker-compose $DESTINATION
+# clone Flectra directory
+git clone --depth=1 https://github.com/minhng92/flectra-2.0-docker-compose $DESTINATION
 rm -rf $DESTINATION/.git
 # set permission
 mkdir -p $DESTINATION/postgresql
@@ -11,9 +11,9 @@ sudo chmod -R 777 $DESTINATION
 # config
 if grep -qF "fs.inotify.max_user_watches" /etc/sysctl.conf; then echo $(grep -F "fs.inotify.max_user_watches" /etc/sysctl.conf); else echo "fs.inotify.max_user_watches = 524288" | sudo tee -a /etc/sysctl.conf; fi
 sudo sysctl -p
-sed -i 's/10017/'$PORT'/g' $DESTINATION/docker-compose.yml
-sed -i 's/20017/'$CHAT'/g' $DESTINATION/docker-compose.yml
-# run Odoo
+sed -i 's/10020/'$PORT'/g' $DESTINATION/docker-compose.yml
+sed -i 's/20020/'$CHAT'/g' $DESTINATION/docker-compose.yml
+# run Flectra
 docker-compose -f $DESTINATION/docker-compose.yml up -d
 
-echo 'Started Odoo @ http://localhost:'$PORT' | Master Password: minhng.info | Live chat port: '$CHAT
+echo 'Started Flectra @ http://localhost:'$PORT' | Master Password: master.password | Live chat port: '$CHAT
